8f5380789d3b600da9c661c4f88f873164a5d705
Yizzy Wu <wut343@newschool.edu>
2017-06-09 12:06:31 -0400

    fixed main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - 100)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
