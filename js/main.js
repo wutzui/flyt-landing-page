06f21d1028e7a46269e3fc2ad7f817c5c065cf12
Yizzy Wu <wut343@newschool.edu>
2017-06-24 15:21:11 -0400

    push updates from gh-pages

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - 100)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -27,5 +27,30 @@
             top: 50
         }
     })
+
+    // white background while scrolling.
+    var scroll_start = 0;
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+    $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top + 100) {
+          $('#mainNav').css('background-color','#FFFFFF')
+                       .css('box-shadow','0 8px 80px rgba(11, 177, 255, 0.2)');
+       } else {
+          $('#mainNav').css('background-color', 'transparent')
+                       .css('box-shadow','0 8px 80px rgba(11, 177, 255, 0)');
+       }
+    });
+
+    var hamburger = $('.nav-mobile');
+    var hamburger_ul = $('#nav-mobile-ul');
+    hamburger.click(function(){
+        // $('.nav-mobile-ul').css('background-color', 'transparent');
+        hamburger_ul.slideToggle( "slow" );  
+    });
+
+
+
 
 })(jQuery); // End of use strict
